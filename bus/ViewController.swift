4ab6ff029643093ccff9293d64acbf23066455e6
gozaing <steganographi@gmail.com>
2016-12-18 15:44:19 +0900

    Initial Commit

---
+++ bus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bus
+//
+//  Created by TOBARU on 2016/12/18.
+//  Copyright © 2016年 TOBARU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
